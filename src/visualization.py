99379fcb497c985f8a44df59e662d2ce064f29b0
Satyam Singh <singhsatyam8383@gmail.com>
2024-07-31 03:51:57 +0530

    new commit

--- src/visualization.py
+++ src/visualization.py
@@ -6,7 +6,7 @@ def plot_stock_comparison(data, title, filename):
         plt.plot(data.index, data[column], label=column)
     plt.title(title)
     plt.xlabel('Date')
-    plt.ylabel('Stock Price')
+    plt.ylabel('Stock Price in USD')
     plt.legend()
     plt.savefig(filename)
     plt.close()
